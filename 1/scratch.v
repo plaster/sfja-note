6a87145deeecbf8342b748d6502fd932bc1ccd39
Haruka Kai <plalal@gmail.com>
2015-12-21 00:48:05 +0900

    Exercise nandb

--- 1/scratch.v
+++ 1/scratch.v
@@ -48,3 +48,37 @@ Definition orb (b1:bool) (b2:bool) : bool :=
   | true => true
   | false => b2
   end.
+
+Example test_orb1: (orb true false) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_orb2: (orb false false) = false.
+Proof. simpl. reflexivity. Qed.
+
+Example test_orb3: (orb false true) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_orb4: (orb true true) = true.
+Proof. simpl. reflexivity. Qed.
+
+(* Exercise nandb *)
+Definition nandb (b1:bool) (b2:bool) : bool :=
+  match b1 with
+  | false => true
+  | true => match b2 with
+    | false => true
+    | true => false
+    end
+  end.
+
+Example test_nandb1: (nandb true false) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_nandb2: (nandb false false) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_nandb3: (nandb false true) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_nandb4: (nandb true true) = false.
+Proof. simpl. reflexivity. Qed.
