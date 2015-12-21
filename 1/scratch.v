b4919d4ea37d94cbc10f93eabb75d79aa7126753
Haruka Kai <plalal@gmail.com>
2015-12-22 02:02:21 +0900

    Exercise andb3

--- 1/scratch.v
+++ 1/scratch.v
@@ -82,3 +82,24 @@ Proof. simpl. reflexivity. Qed.
 
 Example test_nandb4: (nandb true true) = false.
 Proof. simpl. reflexivity. Qed.
+
+Definition andb3 (b1:bool) (b2:bool) (b3:bool) : bool :=
+  match b1 with
+  | false => false
+  | true => match b2 with
+    | false => false
+    | true => b3
+    end
+  end.
+
+Example test_andb31: (andb3 true true true) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_andb32: (andb3 false true true) = false.
+Proof. simpl. reflexivity. Qed.
+
+Example test_andb33: (andb3 true false true) = false.
+Proof. simpl. reflexivity. Qed.
+
+Example test_andb34: (andb3 true true false) = false.
+Proof. simpl. reflexivity. Qed.
