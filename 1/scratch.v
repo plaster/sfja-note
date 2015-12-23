9807bbf55fbe3dc211abe2df211a54cf1514e83c
Haruka Kai <plalal@gmail.com>
2015-12-24 01:05:44 +0900

    factorial

--- 1/scratch.v
+++ 1/scratch.v
@@ -152,5 +152,50 @@ Proof. simpl. reflexivity. Qed.
 Example test_oddb2: (oddb (S (S (S (S O))))) = false.
 Proof. simpl. reflexivity. Qed.
 
+Fixpoint plus (n:nat) (m:nat) : nat :=
+match n with
+| O => m
+| S n' => plus n' (S m)
+end.
+
+Eval simpl in (plus (S (S (S O))) (S (S O)) ).
+
+Fixpoint mult (n m : nat) : nat :=
+match n with
+| O => O
+| S n' => plus m (mult n' m)
+end.
+
+Fixpoint minus (n m : nat) : nat :=
+match n, m with
+| O, _ => O
+| _, O => n
+| S n', S m' => minus n' m'
+end.
+
+Fixpoint exp (base power : nat) : nat :=
+match power with
+| O => S O
+| S power' => mult base (exp base power')
+end.
+
+Example test_mult1: (mult 3 3) = 9.
+Proof. simpl. reflexivity. Qed.
+
+Fixpoint factorial (n:nat) : nat :=
+match n with
+| O => S O
+| S n' => mult n (factorial n')
+end.
+
+Example test_factorial1: (factorial 3) = 6.
+Proof. simpl. reflexivity. Qed.
+
+Example test_factorial2: (factorial 5) = (mult 10 12).
+Proof. simpl. reflexivity. Qed.
+
+
+
+
 
 End Playground2.
